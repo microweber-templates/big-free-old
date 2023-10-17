8f3ae3ae20c244269f713f591870555b708ecc81
Bozhidar Slaveykov <bobi@microweber.com>
2023-10-17 17:48:27 +0300

    update

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $config = array();
 $config['name'] = "Big Template - FREE VERSION";
 $config['author'] = "Microweber CMS";
-$config['version'] = "6.4";
+$config['version'] = "6.5";
 $config['url'] = "http://microweber.com";
 
 
